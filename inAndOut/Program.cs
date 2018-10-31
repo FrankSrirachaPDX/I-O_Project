387e5128b9bdb9267ce87b6237548ecc7e4907e6
Ryan <werner.ryan18@gmail.com>
2018-10-31 12:32:36 -0700

    Add project files.

---
+++ inAndOut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inAndOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inPut;
+
+            inPut = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter sw = new StreamWriter("C:\\Users\\Ryan\\source\\repos\\inAndOut\\inAndOut\\log\\text.txt", true))
+            {
+                sw.WriteLine(inPut);
+                
+            }
+
+            string outPut = File.ReadAllText("C:\\Users\\Ryan\\source\\repos\\inAndOut\\inAndOut\\log\\text.txt");
+            Console.WriteLine(outPut);
+
+
+            Console.ReadLine();
+        }
+    }
+}
